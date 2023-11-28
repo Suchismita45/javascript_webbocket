6d93224f64a9db176bd5addf97499e6050455c4c
Suchismita45 <151999829+Suchismita45@users.noreply.github.com>
2023-11-28 10:18:34 +0000

    Introductionfile saved

---
+++ 01-basics/test.js
@@ -0,0 +1,7 @@
+console.log("hello")
+/*1.Java Script is a high level scripting language to use on developing web and movie application efficiently.
+2.We used javascript on client side as well as server side.
+3.In javascript we do logical implementation in web pages.
+4.In today era we use ES6 version of javascript ,as it is latest one.
+*ES6-Echma Script6(Vanilla javascript)
+*/
